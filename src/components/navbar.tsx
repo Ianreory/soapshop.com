840a74fe60e0fa4d176a7167494ee0d8d8bc8a26
Ianroery <ianroeri@gmail.com>
2024-07-22 07:21:55 +0700

    solve confiq

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import {  Menu, Package2, Search } from "lucide-react";
+import { Menu, Package2, Search } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
@@ -16,12 +16,11 @@ const Navbar = () => {
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b  px-4 md:px-6 z-50 bg-[#1E293B]">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 py-12">
-        <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
-          <img src="/public/photo.png" alt="" width={90} height={90} />
-          <span className="sr-only">Shop</span>
+        <Link to="/" className="flex items-center ">
+          <h1 className="sr-only gap-2 font-bold font-Poppins text-4xl ">Soap Shop</h1>
         </Link>
         <Link to="/" className="text-muted-foreground transition-colors hover:text-foreground">
-          Shop
+          <h1 className="font-bold font-Poppins text-3xl "> Soap</h1>
         </Link>
       </nav>
       <Sheet>
